f13901ff3b5cc414ec117b1822c2cab88f50adf9
권오영 <kwono8693@naver.com>
2026-03-10 18:44:15 +0900

    260310 add2

--- js/index.js
+++ js/index.js
@@ -16,4 +16,19 @@ document.addEventListener("DOMContentLoaded",function(){
             disableOnInteraction: false,
         },
     });
+    const tabs = document.querySelectorAll(".tabs>span");
+    const tabDes = document.querySelectorAll(".tab-menu>ul");
+
+    tabs.forEach(function(span,i){
+        span.addEventListener("click",function(){
+            tabs.forEach(tab=>tab.classList.remove("active"))
+            // 3개의 span 태그에 있는 active를 다 지운다.
+            span.classList.add("active")
+            // 클릭한 span 태그에 active 클래스가 추가된다.
+            tabDes.forEach(ul=>ul.classList.remove("active"))
+            tabDes[i].classList.add("active")
+        })
+
+    })
+
 })
