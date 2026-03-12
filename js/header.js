921e8120bb8e9a8871150f213487ff7704f9bd3b
권오영 <kwono8693@naver.com>
2026-03-12 17:51:44 +0900

    260312 add 1

--- js/header.js
+++ js/header.js
@@ -31,4 +31,34 @@ function initHeaderMenu() {
     });
   });
 
+  // 메뉴버튼을 누르면 header가 나오는 기능
+    
+    const btnMenu = document.querySelector(".btn-menu");
+    const menuSmartHidden = document.querySelector(".menu-smart-hidden")
+    const btnClose = document.querySelector(".btn-close")
+    const gnbSmartList = document.querySelectorAll(".gnb-smart>li")
+
+    btnMenu.addEventListener("click", ()=>{
+        menuSmartHidden.classList.add("on")
+    })
+    btnClose.addEventListener("click", ()=>{
+        menuSmartHidden.classList.remove("on")
+        gnbSmartList.forEach(tag=>tag.classList.remove("on"))
+
+    })
+    // 모바일에서 2뎁스 메뉴 나오는 기능
+
+    const btnMore = document.querySelectorAll(".gnb-smart li .btn-more")
+    btnMore.forEach(span=>{
+        span.addEventListener("click",()=>{
+            const parent = span.closest("li");
+            const isActive = parent.classList.contains("on");
+
+            gnbSmartList.forEach(li => li.classList.remove("on"));
+
+            if (!isActive) {
+                parent.classList.add("on");
+            }
+        })
+    })
 }
