9539aaf9a689a4bf19035a133a5e6c5a8ea74497
권오영 <kwono8693@naver.com>
2026-03-17 16:37:43 +0900

    260317 add

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     
     // 1. 메인 배너 슬라이더 (station)
-    const swiper1 = new Swiper('.station', {
+    const swiper1 = new Swiper('.station-banner', {
         direction: 'horizontal',
         loop: true,
         speed: 1000,
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
         },
     });
 
-
+   // 2. 탭 메뉴
     const tabs = document.querySelectorAll(".tabs>span");
     const tabDes = document.querySelectorAll(".tab-menu>ul");
 
@@ -32,4 +32,37 @@ document.addEventListener("DOMContentLoaded", function () {
 
     })
 
+    // 3. 팝업 메뉴
+    const train = document.querySelector('.train-popup'); // 수정한 HTML의 .train 선택
+    let count = 0;
+
+    console.log(train)
+
+    setInterval(() => {
+        count++;
+        if (count > 2) { count = 0; }
+        train.style.transform = `translateX(${-33.333 * count}%)`;
+    }, 2500);
+
+    // 요소 선택
+    const searchTrigger = document.querySelector('.search-trigger');
+    const searchWindow = document.querySelector('.search-window');
+    const closeBtn = document.querySelector('.close-btn');
+
+    // 1. 이미지 클릭 시 검색창 나타남
+    searchTrigger.addEventListener('click', () => {
+        searchWindow.classList.add('active');
+    });
+
+    // 2. 닫기 버튼 클릭 시 검색창 사라짐
+    closeBtn.addEventListener('click', () => {
+        searchWindow.classList.remove('active');
+    });
+
+    // (선택) 배경을 클릭해도 닫히게 하고 싶다면?
+    window.addEventListener('click', (e) => {
+        if (e.target === searchWindow) {
+            searchWindow.classList.remove('active');
+        }
+    });
 });
